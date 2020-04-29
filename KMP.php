bb8f5cf2a788f3dc9e9f54c3e961b04651ec18d4
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2020-04-30 05:09:07 +0700

    Create KMP.php

---
+++ KMP.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <title>tubes</title>
+
+</head>
+<style>
+  h2 {
+    text-align: center;
+  }
+
+  p {
+    text-align: justify;
+  }
+</style>
+
+<body>
+  <h2>KNUTH MORRIS PRATT</h2>
+  <h2>Pasien Corona Meninggal di Indonesia</h2>
+  <h2>Hari Ini Jadi 240, Sembuh 222</h2>
+  <br>
+
+  <div align="center">
+    <div class="card mb-3" style="max-width: 1300px;">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img src="corona.jpeg" class="card-img" alt="" width="125px" height="296px"><br>
+          <!-- Running Time -->
+         
+        </div>
+        <div class="col-md-8">
+          <div class="card-body" id="kalimat">
+            <p class="card-text">Jakarta - Pemerintah mengumumkan kembali jumlah pasien kasus positif virus Corona
+              yang meninggal dunia di wilayah Indonesia. Total ada 240 pasien Corona yang meninggal dunia.
+              "Ada penambahan kasus meninggal sebanyak 19 orang sehingga total kasus meninggal adalah 240 orang,
+              " kata juru bicara pemerintah terkait penanganan wabah Corona, Achmad Yurianto,
+              dalam konferensi pers yang ditayangkan BNPB di YouTube, Rabu (8/4/2020).
+              Total pasien sembuh juga bertambah 18 orang, sehingga total pasien sembuh dari virus Corona di
+              Indonesia hari ini menjadi 222 orang.
+              Yuri menyebut pasien yang dinyatakan negatif ini telah dites dua kali.
+              "Jumlah kasus yang sembuh meningkat 18 orang sehingga total menjadi 222 orang," ujar Yuri.</p>
+            <p class="card-text">Hingga hari ini, tercatat ada 2.956 kasus positif Corona di Indonesia.
+              Jumlah tersebut bertambah 218 dari angka sebelumnya diketahui,
+              per 7 April 2020 pemerintah mengumumkan ada 221 orang meninggal akibat virus Corona.
+              Sementara pasien sembuh 204 orang.</p>
+
+            <body>
+              <div class="form-group">
+                <form method="post">
+                  <input type="text" class="form-control mt-3 mb-3" style="width: 300px" name="searchKMP" placeholder="Search..." required />
+                  <input type="submit" class="btn btn-info" name="add" value="Search" />
+                </form>
+              </div>
+              <hr>
+            </body>
+</body>
+
+</html>
